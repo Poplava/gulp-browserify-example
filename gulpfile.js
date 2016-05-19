036135b3a1720169c5ca07d443ed0736457cb820
Ihor Poplavskyi <igor.poplava@gmail.com>
2016-05-19 13:24:10 +0300

    removed useless

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 
-// Basic usage 
 gulp.task('default', function() {
-  // Single entry point to browserify
+
   gulp.src('src/app.js')
-    .pipe(browserify({
-      insertGlobals : true
-    }))
+    .pipe(browserify())
     .pipe(gulp.dest('./build'))
+
 });
